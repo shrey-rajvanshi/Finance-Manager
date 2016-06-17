67be6b29f153460e11af7ba0e285f9b4a6f7ddfe
shrey-rajvanshi <shreykantrajvanshi@gmail.com>
2016-06-17 08:35:32 +0000

    fixed indentation, added requirements.txt, initial commit for client side app

--- fmapp/views.py
+++ fmapp/views.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 import flask
 import json
-from fmapp import app,db
+from fmapp import app, db
 from fmapp.models import *
 
 
@@ -9,6 +9,7 @@ from fmapp.models import *
 @app.route('/<path:path>')
 def catch_all(path):
     return 'Looking for %s ?' % path
+
 
 @app.route('/shrey', methods=['GET', 'POST'])
 def hello_shrey():
@@ -19,55 +20,68 @@ def hello_shrey():
 
 @app.route('/wallets', methods=['GET'])
 def get_wallets():
-	wallets = Wallet.query.all()
-	return json.dumps([x.json_dump() for x in wallets])
+    wallets = Wallet.query.all()
+    return json.dumps([x.json_dump() for x in wallets])
+
 
 @app.route('/add/wallets/<name>', methods=['GET'])
 def add_empty_wallet(name):
-	wallet = Wallet()
-	wallet.name=name
-	wallet.balance=0
-	db.session.add(wallet)
-	db.session.commit()
-	return json.dumps(wallet.json_dump())
+    wallet = Wallet()
+    wallet.name = name
+    wallet.balance = 0
+    db.session.add(wallet)
+    db.session.commit()
+    return json.dumps(wallet.json_dump())
+
 
 @app.route('/add/wallets/<name>/<int:balance>', methods=['GET'])
-def add_wallet(name,balance):
-	wallet = Wallet()
-	wallet.name=name
-	wallet.balance=balance
-	db.session.add(wallet)
-	db.session.commit()
-	return json.dumps(wallet.json_dump())
+def add_wallet(name, balance):
+    wallet = Wallet()
+    wallet.name = name
+    wallet.balance = balance
+    db.session.add(wallet)
+    db.session.commit()
+    return json.dumps(wallet.json_dump())
+
 
 @app.route('/getbalance/<wallet>/', methods=['GET'])
 def getbalance(wallet):
-	wallet = Wallet.query.filter(Wallet.name == wallet).first()
-	return str(wallet.balance)
+    wallet = Wallet.query.filter(Wallet.name == wallet).first()
+    return str(wallet.balance)
 
 
 #Transaction URLs
 
+
 @app.route('/transactions', methods=['GET'])
 def get_transactions():
-	transactions = Transaction.query.all()
-	return json.dumps([x.json_dump() for x in transactions])
+    transactions = Transaction.query.all()
+    return json.dumps([x.json_dump() for x in transactions])
+
 
 @app.route('/add/<name>/<int:amount>/<int:wallet_id>/<int:category_id>', methods=['GET'])
-def deposit(name,amount,wallet_id,category_id):
-	transaction = Transaction(name,amount,wallet_id,category_id)
-	wallet=Wallet.query.get(wallet_id)
-	wallet.balance += int(amount)
-	db.session.add(wallet)
-	db.session.add(transaction)
-	db.session.commit()
-	return "CREated"
+def deposit(name, amount, wallet_id, category_id):
+    transaction = Transaction(name, amount, wallet_id, category_id)
+    wallet = Wallet.query.get(wallet_id)
+    wallet.balance += int(amount)
+    db.session.add(wallet)
+    db.session.add(transaction)
+    db.session.commit()
+    return "Created"
+
 
 @app.route('/sub/<name>/<int:amount>/<int:wallet_id>/<int:category_id>', methods=['post'])
-def withdraw(name,amount,wallet_id,category_id):
-	transaction = Transaction(name,amount,wallet_id,category_id)
-	wallet=Wallet.query.get(wallet_id)
-	wallet.balance -= int(amount)
-	db.session.add(wallet)
-	db.session.add(transaction)
-	db.session.commit()
+def withdraw(name, amount, wallet_id, category_id):
+    transaction = Transaction(name, amount, wallet_id, category_id)
+    wallet = Wallet.query.get(wallet_id)
+    wallet.balance -= int(amount)
+    db.session.add(wallet)
+    db.session.add(transaction)
+    db.session.commit()
+
+
+# UI URLs
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html', name="shrey")
--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from fmapp import app
 
 
 if __name__ == '__main__':
-	app.secret_key = 'super secret keyjkhogh08923epuoij'
-	app.run(debug=True)
+    app.secret_key = 'super secret keyjkhogh08923epuoij'
+    app.run(debug=True)
