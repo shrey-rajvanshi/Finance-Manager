76caf66fc57814fbd3dd8b71a61df2b5ff0316ea
shrey-rajvanshi <shreykantrajvanshi@gmail.com>
2016-07-08 12:21:52 +0000

    changes CSS

--- fmapp/__init__.py
+++ fmapp/__init__.py
@@ -5,6 +5,8 @@ from flask_admin import Admin
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/vagrant/www/Finance-Manager/fm.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/fmn'
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.secret_key = "SUperSecretKeyHAHAHA123213-12"
 db = SQLAlchemy(app)
 
 admin = Admin(app, name='Welcome to Shrey\'s  Finance Manager', template_mode='bootstrap3')
