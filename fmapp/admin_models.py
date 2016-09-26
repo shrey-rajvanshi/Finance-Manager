dcd8297cc421e91328a97a381f13547f909ee399
shrey-rajvanshi <shreykantrajvanshi@gmail.com>
2016-09-26 14:52:41 +0530

    Adds date descending sort on admin transactions

--- fmapp/admin_models.py
+++ fmapp/admin_models.py
@@ -2,6 +2,11 @@ from fmapp import app,admin
 from fmapp.models import *
 from flask_admin.contrib.sqla import ModelView
 
+class TransactionAdmin(ModelView):
+        column_list = ('name', 'amount','date')
+        column_sortable_list = ('date')
+        column_default_sort = ('date', True)
+
 admin.add_view(ModelView(Wallet, db.session))
 admin.add_view(ModelView(Category, db.session))
 admin.add_view(ModelView(Transaction, db.session))
