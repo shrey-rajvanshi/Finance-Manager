0d06ee17b6477679bfe170da6911d41464de3b9e
shrey-rajvanshi <shreykantrajvanshi@gmail.com>
2016-11-24 08:16:50 +0000

    adds view for bored

--- fmapp/clientviews.py
+++ fmapp/clientviews.py
@@ -12,5 +12,16 @@ def home():
     transactions = json.dumps([x.json_dump() for x in transactions_list])
     categories_list = Category.query.all()
     categories = json.dumps([x.json_dump() for x in categories_list])
+
     return render_template('home.html', name="Shrey", transactions=json.loads(transactions),
-                            categories=json.loads(categories))
+                           categories=json.loads(categories))
+
+
+@app.route('/bored', methods=['GET'])
+def bored():
+    transactions = json.dumps([x.json_dump() for x in Transaction.query.order_by(desc(Transaction.date))])
+    categories = json.dumps([x.json_dump() for x in Category.query.all()])
+    tags = json.dumps([x.json_dump() for x in Tags.query.all()])
+
+    return render_template('bored.html', name="Bored", transactions=json.loads(transactions),
+                           categories=json.loads(categories), tags=json.loads(tags))
