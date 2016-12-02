2282f8b92db5f60b85ff1f0b3e0574cab7ef0822
shrey-rajvanshi <shreykantrajvanshi@gmail.com>
2016-12-02 12:13:03 +0000

    Adds Csrf and local config

--- fmapp/__init__.py
+++ fmapp/__init__.py
@@ -3,9 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_wtf.csrf import CsrfProtect  
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/vagrant/www/Finance-Manager/fm.db'
+#app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////Users/practo/Desktop/project/fmn.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/fmn'
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 
@@ -16,6 +18,6 @@ migrate = Migrate(app, db)
 admin = Admin(app, name='Welcome to Finance Manager', template_mode='bootstrap3')
 login_manager = LoginManager()
 login_manager.init_app(app)
-
+csrf = CsrfProtect(app) 
 
 from fmapp import views, models, admin_models, clientviews
