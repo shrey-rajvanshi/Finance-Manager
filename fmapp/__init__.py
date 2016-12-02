58d1d05c5c680dec2bb9b5207431ea0552d2b762
shrey-rajvanshi <shreykantrajvanshi@gmail.com>
2016-12-02 12:14:22 +0000

    removes migrations extension

--- fmapp/__init__.py
+++ fmapp/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
-from flask_migrate import Migrate
+#rom flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_wtf.csrf import CsrfProtect  
 
@@ -14,7 +14,7 @@ app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.secret_key = "SUperSecretKeyHAHAHA123213-12"
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+#migrate = Migrate(app, db)
 admin = Admin(app, name='Welcome to Finance Manager', template_mode='bootstrap3')
 login_manager = LoginManager()
 login_manager.init_app(app)
